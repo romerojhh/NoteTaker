95064e812c7b0db0fc2752127c9032f2c27e1a70
romerojhh <61641738+MrSaladdd@users.noreply.github.com>
2024-11-14 19:37:29 -0800

    add custom application

---
+++ app/src/main/java/com/romerojhh/notetaker/NoteTakerApplication.kt
@@ -0,0 +1,10 @@
+package com.romerojhh.notetaker
+
+import android.app.Application
+
+class NoteTakerApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // TODO: Initialize any dependencies or libraries here
+    }
+}
