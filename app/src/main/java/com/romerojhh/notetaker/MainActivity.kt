ae8496c9f98325e808ebc4bd3660969161e836a9
romerojhh <61641738+MrSaladdd@users.noreply.github.com>
2024-11-15 23:38:00 -0800

    Add navigation example + update dependencies

--- app/src/main/java/com/romerojhh/notetaker/MainActivity.kt
+++ app/src/main/java/com/romerojhh/notetaker/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.romerojhh.notetaker.sample.navigation.NavigationExample
 import com.romerojhh.notetaker.ui.theme.NoteTakerTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             NoteTakerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    NavigationExample(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
---
+++ app/src/main/java/com/romerojhh/notetaker/sample/navigation/Navigation.kt
@@ -0,0 +1,79 @@
+package com.romerojhh.notetaker.sample.navigation
+
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.slideInVertically
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Profile(val name: String)
+
+@Serializable
+object FriendsList
+
+// Define the ProfileScreen composable.
+@Composable
+fun ProfileScreen(
+    profile: Profile,
+    onNavigateToFriendsList: () -> Unit
+) {
+    Column {
+        Text("Profile for ${profile.name}")
+        Button(onClick = { onNavigateToFriendsList() }) {
+            Text("Go to Friends List")
+        }
+    }
+}
+
+// Define the FriendsListScreen composable.
+@Composable
+fun FriendsListScreen(onNavigateToProfile: () -> Unit) {
+    Column {
+        Text("Friends List")
+        Button(onClick = { onNavigateToProfile() }) {
+            Text("Go to Profile")
+        }
+    }
+}
+
+// Define the MyApp composable, including the `NavController` and `NavHost`.
+@Composable
+fun NavigationExample(modifier: Modifier = Modifier) {
+    val navController = rememberNavController()
+    NavHost(modifier = modifier, navController = navController, startDestination = Profile(name = "John Smith")) {
+        composable<Profile> (
+            enterTransition = {
+                slideInVertically(
+                    initialOffsetY = { it },
+                    animationSpec = tween(durationMillis = 700, easing = LinearOutSlowInEasing)
+                )
+            }
+        ) { backStackEntry ->
+            val profile: Profile = backStackEntry.toRoute()
+            ProfileScreen(
+                profile = profile,
+                onNavigateToFriendsList = {
+                    navController.navigate(route = FriendsList)
+                }
+            )
+        }
+        composable<FriendsList> {
+            FriendsListScreen(
+                onNavigateToProfile = {
+                    navController.navigate(
+                        route = Profile(name = "Aisha Devi")
+                    )
+                }
+            )
+        }
+    }
+}
